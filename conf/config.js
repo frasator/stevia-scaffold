feca17445f36906a7dd1d2d622fde9a1122c0e8a
Edgar S. Hurtado <edsanhu@gmail.com>
2016-04-14 12:07:29 +0200

    Make room for stv-launcher in header

--- conf/config.js
+++ conf/config.js
@@ -3,4 +3,4 @@
  * Changes to this file may cause the application does not work as it should
  **/
 STEVIA_MANAGER_LOG = true;
-STEVIA_SERVER_HOST = 'http://localhost:5555';
+STEVIA_SERVER_HOST = 'http://mem18:5555';

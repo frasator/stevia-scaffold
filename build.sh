332ff0a0e26fb2364bc40f2530f3376b0d2d71a7
frasator <frasator@gmail.com>
2016-05-20 13:25:23 +0200

    Updated stevia-elements path.

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+NAME="application"
+ELEMENT="application-element"
+BP=build
+
+rm -rf $BP
+mkdir -p $BP
+mkdir -p $BP/fontawesome
+mkdir -p $BP/webcomponentsjs
+
+vulcanize --inline-scripts --inline-css --strip-comments $ELEMENT.html > $BP/$ELEMENT.html
+
+cp -r $NAME-index.html $BP/index.html
+cp -r conf/ $BP/
+cp -r bower_components/stevia-elements/fonts/ $BP/
+cp -r bower_components/stevia-elements/css/ $BP/
+cp -r bower_components/fontawesome/css $BP/fontawesome/
+cp -r bower_components/fontawesome/fonts $BP/fontawesome/
+cp -r bower_components/webcomponentsjs/*.min.js $BP/webcomponentsjs/
+
+#
+# fix index.html css paths
+#
+sed -i s@'bower_components/stevia-elements/'@@g $BP/index.html
+sed -i s@'bower_components/'@@g $BP/index.html
+## end fix paths

61832992fee024981b17b89415971ec32e0e1006
frasator <frasator@gmail.com>
2016-11-04 15:45:15 +0100

    Updates.

--- conf/config.js
+++ conf/config.js
@@ -3,4 +3,8 @@
  * Changes to this file may cause the application does not work as it should
  **/
 STEVIA_MANAGER_LOG = true;
-STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01/';
+STEVIA_SERVER_HOST = 'http://localhost:5555';
+
+STEVIA_SHOW_JOB_ERROR_FILES = true;
+UPLOAD_FILES_WITH_ERRORS = true;
+STEVIA_SHOW_HIDDEN_FILES = true;

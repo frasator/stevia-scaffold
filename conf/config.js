a54e41743417b11a2d3faf7e05d343356ea22931
frasator <frasator@gmail.com>
2017-03-23 15:55:08 +0100

    Updated license.

--- conf/config.js
+++ conf/config.js
@@ -3,8 +3,9 @@
  * Changes to this file may cause the application does not work as it should
  **/
 STEVIA_MANAGER_LOG = true;
-STEVIA_SERVER_HOST = 'http://localhost:5555';
+// STEVIA_SERVER_HOST = 'http://localhost:5555';
+STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
 
 STEVIA_SHOW_JOB_ERROR_FILES = true;
 UPLOAD_FILES_WITH_ERRORS = true;
-STEVIA_SHOW_HIDDEN_FILES = true;
+STEVIA_SHOW_HIDDEN_FILES = false;
